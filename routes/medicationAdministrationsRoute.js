198e286d0e4dd013b3441d83c134a3d97cf856f0
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-07-27 13:59:49 +0100

    search for a patients medication administration

--- routes/medicationAdministrationsRoute.js
+++ routes/medicationAdministrationsRoute.js
@@ -24,4 +24,26 @@ medicationAdministrationsRouter.get("/", (req, res, next) => {
     .then((data) => res.json(data));
 });
 
+medicationAdministrationsRouter.get("/:searchID", (req, res, next) => {
+  const searchID = req.params.searchID;
+  const request = "/MedicationAdministration?patient=" + searchID;
+
+  const requestType = "GET";
+
+  const headers = {
+    "x-api-key": process.env.X_API_KEY,
+    Authorization: "Bearer " + process.env.ID_TOKEN,
+  };
+
+  const requestOptions = {
+    method: requestType,
+    headers: headers,
+    redirect: "follow",
+  };
+
+  fetch(process.env.API_URL + request, requestOptions)
+    .then((response) => response.json())
+    .then((data) => res.json(data));
+});
+
 export default medicationAdministrationsRouter;
