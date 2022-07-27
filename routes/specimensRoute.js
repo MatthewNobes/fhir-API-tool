8b72f20efb0a6b8d7cdf78c45a5ef793ee829d47
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-07-27 12:47:46 +0100

    specimen for a specific user

--- routes/specimensRoute.js
+++ routes/specimensRoute.js
@@ -24,4 +24,26 @@ specimensRouter.get("/", (req, res, next) => {
     .then((data) => res.json(data));
 });
 
+specimensRouter.get("/:searchID", (req, res, next) => {
+  const searchID = req.params.searchID;
+  const request = "/Specimen?patient=" + searchID;
+
+  const requestType = "GET";
+
+  const headers = {
+    "x-api-key": process.env.X_API_KEY,
+    Authorization: "Bearer " + process.env.ID_TOKEN,
+  };
+
+  const requestOptions = {
+    method: requestType,
+    headers: headers,
+    redirect: "follow",
+  };
+
+  fetch(process.env.API_URL + request, requestOptions)
+    .then((response) => response.json())
+    .then((data) => res.json(data));
+});
+
 export default specimensRouter;
